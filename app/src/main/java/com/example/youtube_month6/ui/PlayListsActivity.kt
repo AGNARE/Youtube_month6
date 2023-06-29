ca293c0fbf39c9f0236303d0c3cba690181de751
Argen <argenbagylov0@gmail.com>
2023-06-29 18:12:17 +0600

    youtube check internet hw2 second commit

--- app/src/main/java/com/example/youtube_month6/ui/PlayListsActivity.kt
+++ app/src/main/java/com/example/youtube_month6/ui/PlayListsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.youtube_month6.ui
 
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.youtube_month6.core.base.BaseActivity
 import com.example.youtube_month6.databinding.ActivityPlaylistsBinding
+import com.example.youtube_month6.internet.ConnectionInternet
 import com.example.youtube_month6.ui.playlistAdapter.AdapterPlayLists
 
 class PlayListsActivity :
@@ -26,6 +28,19 @@ class PlayListsActivity :
             adapter.setList(it.items)
             binding.recyclerView.adapter = adapter
             Toast.makeText(this, it.kind.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun checkInternet() {
+        super.checkInternet()
+        ConnectionInternet(application).observe(this){ isConnection ->
+            if (isConnection){
+                binding.noInternet.visibility = View.GONE
+                binding.yesInternet.visibility = View.VISIBLE
+            } else{
+                binding.noInternet.visibility = View.VISIBLE
+                binding.yesInternet.visibility = View.GONE
+            }
         }
     }
 }
